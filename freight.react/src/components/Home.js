282b75dd2a26886e6424d282bd60c7e7fa1e2279
vincefio <vincentfiorilli@gmail.com>
2019-02-17 09:39:08 -0500

    background pic

--- freight.react/src/components/Home.js
+++ freight.react/src/components/Home.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import fire from '../config/firebase';
+import Truck from '../img/freightPic2.jpg';
 
 export default class Home extends Component {
     constructor(props){
@@ -14,6 +15,12 @@ export default class Home extends Component {
   render() {
     return (
       <div>
+            <header>
+                <div className="headerWrap">
+                    <img className="backgroundImg" src={Truck} alt="truck"></img>
+                </div>
+            </header>
+
             <h1>WERE KILLIN THIS SHIT HOMEEEE</h1>
             <h2>Hello {this.props.user}</h2>
             <button onClick={this.signOut}>Log Out</button>
