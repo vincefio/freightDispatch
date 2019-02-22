a37a49224fd0257fab3e7eb20a13a454a2013333
vincefio <vincentfiorilli@gmail.com>
2019-02-21 20:00:49 -0500

    more form

--- freight.react/src/components/HireCompany.js
+++ freight.react/src/components/HireCompany.js
@@ -13,9 +13,22 @@ export default class HireCompany extends Component {
         phone: '',
         email: '',
         receiverCompanyName: '',
-
+        receiverAddress: '',
+        receiverCity: '',
+        receiverState: '',
+        receiverZip: '',
+        receiverPhone: ''
       }
+
+      this.handleChange = this.handleChange.bind(this)
   }
+
+  handleChange(event){
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  }
+
   render() {
     return (
       <div className="container hireFormContainer">
@@ -24,29 +37,37 @@ export default class HireCompany extends Component {
 <form>
         <h3>My Information</h3>
           <label for="pickUpDate">Pick Up Date</label>
-          <input value={this.state.pickUpDate} id="pickUpDate" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="pickUpDate" className="formInput" type="text"></input>
           <label for="myCompanyName">Company Name</label>
-          <input value={this.state.myCompanyName} id="myCompanyName" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="myCompanyName" className="formInput" type="text"></input>
           <label for="address">Address</label>
-          <input value={this.state.address} id="address" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="address" className="formInput" type="text"></input>
           <label for="city">City</label>
-          <input value={this.state.city} id="city" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="city" className="formInput" type="text"></input>
           <label for="state">State</label>
-          <input value={this.state.state} id="state" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="state" className="formInput" type="text"></input>
           <label for="zip">Zip</label>
-          <input value={this.state.zip} id="zip" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="zip" className="formInput" type="text"></input>
           <label for="phone">Phone</label>
-          <input value={this.state.phone} id="phone" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="phone" className="formInput" type="text"></input>
           <label for="email">Email</label>
-          <input value={this.state.email} id="email" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="email" className="formInput" type="text"></input>
         </form>
    
         <form>
         <h3>Receiver Information</h3>
           <label for="receiverCompanyName">Company Name</label>
-          <input value={this.state.receiverCompanyName} id="receiverCompanyName" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="receiverCompanyName" className="formInput" type="text"></input>
           <label for="receiverAddress">Address</label>
-          <input value={this.state.receiverAddress} id="receiverAddress" className="formInput" type="text"></input>
+          <input onClick={this.handleChange} id="receiverAddress" className="formInput" type="text"></input>
+          <label for="receiverCity">City</label>
+          <input onClick={this.handleChange} id="receiverCity" className="formInput" type="text"></input>
+          <label for="receiverState">State</label>
+          <input onClick={this.handleChange} id="receiverState" className="formInput" type="text"></input>
+          <label for="receiverZip">State</label>
+          <input onClick={this.handleChange} id="receiverZip" className="formInput" type="text"></input>
+          <label for="receiverPhone">Phone</label>
+          <input onClick={this.handleChange} id="receiverPhone" className="formInput" type="text"></input>
         </form> 
 
 
