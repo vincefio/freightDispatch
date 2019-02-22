aecc17bebe41a5af0921018d2ba7d1133fe39ed3
vincefio <vincentfiorilli@gmail.com>
2019-02-21 19:15:30 -0500

    react form

--- freight.react/src/components/HireCompany.js
+++ freight.react/src/components/HireCompany.js
@@ -1,10 +1,31 @@
 import React, { Component } from 'react'
 
 export default class HireCompany extends Component {
+  constructor(props){
+    super(props)
+      this.state = {
+        pickUpDate: '',
+
+      }
+  }
   render() {
     return (
-      <div>
-        <h1>HIRE</h1>
+      <div className="container hireFormContainer">
+
+       
+<form>
+        <h2>Create Shipment</h2>
+          <label for="pickUpDate">Pick Up Date</label>
+          <input value={this.state.pickUpDate} id="pickUpDate" className="formInput" type="text"></input>
+        </form>
+   
+        <form>
+        <h2>Create Shipment</h2>
+          <label for="pickUpDate">Pick Up Date</label>
+          <input value={this.state.pickUpDate} id="pickUpDate" className="formInput" type="text"></input>
+        </form> 
+
+
       </div>
     )
   }
