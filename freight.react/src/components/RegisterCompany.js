c5f94a82a68e859b7da6c8a7f944ca442cfb511e
vincefio <vincentfiorilli@gmail.com>
2019-02-26 05:55:56 -0500

    valication

--- freight.react/src/components/RegisterCompany.js
+++ freight.react/src/components/RegisterCompany.js
@@ -4,13 +4,28 @@ import firebase from 'firebase'
 export default class RegisterCompany extends Component {
   componentDidMount(){
      // Get a reference to the database service
-    var database = firebase.database();
+    //var database = firebase.database();
+    var database = firebase.firestore();
 
-    database.ref('companies/' + 20).set({
+    /*database.collection("companies").add({
+      first: "Ada",
+      last: "Lovelace",
+      born: 1815
+  })
+  .then(function(docRef) {
+      console.log("Document written with ID: ", docRef.id);
+  })
+  .catch(function(error) {
+      console.error("Error adding document: ", error);
+  });*/
+
+    /*database.ref('companies/' + 20).set({
       name: 'Johnson',
     });
 
-    console.log(database.ref('companies/'))
+    console.log(database.ref('companies/'))*/
+
+  //MAKE A CLASS FOR A NEW COMPANY?  
   }
 
   render() {
