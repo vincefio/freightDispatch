77b3ae7073322e3787b4b7f7fa2e115d996306ba
vincefio <vincentfiorilli@gmail.com>
2019-03-02 11:39:31 -0500

    form validation

--- freight.react/src/components/RegisterCompany.js
+++ freight.react/src/components/RegisterCompany.js
@@ -10,7 +10,14 @@ export default class RegisterCompany extends Component {
       registerCompanyAddress: '',
       registerCompanyCity: '',
       registerCompanyState: '',
-      registerCompanyZip: ''
+      registerCompanyZip: '',
+      formErrors: {
+        registerCompanyName: '',
+        registerCompanyAddress: '',
+        registerCompanyCity: '',
+        registerCompanyState: '',
+        registerCompanyZip: '',
+      }
     }
   }
 
@@ -25,29 +32,79 @@ export default class RegisterCompany extends Component {
     this.setState({
       [event.target.name]: event.target.value
     })
+
+    //validate inputs
+    const { name, value } = event.target
+    let { formErrors } = this.state
+
+    switch(name){
+      case 'registerCompanyName':
+        formErrors.registerCompanyName = value.length < 3 ? 'Please add a valid company name' : '';
+        break;
+      case 'registerCompanyAddress':
+        formErrors.registerCompanyAddress = value.length < 3 ? 'Please add a valid company address' : '';
+        break;
+      case 'registerCompanyName':
+        formErrors.registerCompanyCity = value.length < 3 ? 'Please add a valid city name' : '';
+        break;
+      case 'registerCompanyCity':
+        formErrors.registerCompanyCity = value.length < 3 ? 'Please add a valid city' : '';
+        break;
+      case 'registerCompanyState':
+        formErrors.registerCompanyState = value.length < 3 ? 'Please add a valid state' : '';
+        break;
+      case 'registerCompanyZip':
+        formErrors.registerCompanyZip = value.length < 3 ? 'Please add a valid zip' : '';
+        break;
+      default:
+        break;
+    }
+
   } 
+
+  handleSubmit = (event) => {
+    event.preventDefault()
+    //console.log(this.state)
+    
+    //Validate form
+ 
+  }
 
 
   render() {
+    const { formErrors } = this.state
     
     return (
       <div className="container">
         <h3>REGISTER COMPANY</h3>
-          <label for="registerCompanyName">Company Name</label>
-          <input onChange={this.handleChange} name="registerCompanyName" id="registerCompanyName" className="" type="text"></input>
-          <label for="registerCompanyAddress">Address</label>
-          <input onChange={this.handleChange} name="registerCompanyAddress" id="registerCompanyAddress" className="formInput" type="text"></input>
-
-          <label for="registerCompanyCity">City</label>
-          <input onChange={this.handleChange} name="registerCompanyCity" id="registerCompanyCity" className="formInput" type="text"></input>
-
-          <label for="registerCompanyState">State</label>
-          <input onChange={this.handleChange} name="registerCompanyState" id="registerCompanyState" className="formInput" type="text"></input>
-   
-          <label for="registerCompanyZip">Zip</label>
-          <input onChange={this.handleChange} name="registerCompanyZip" id="registerCompanyZip" className="formInput" type="text"></input>
-
-  
+          <form onSubmit={this.handleSubmit}>
+            <label for="registerCompanyName">Company Name</label>
+            <input onChange={this.handleChange} name="registerCompanyName" id="registerCompanyName" className="" type="text"></input>
+            {
+              formErrors.registerCompanyName ? <span className="errorMessage" data-error="wrong" data-success="right">{formErrors.registerCompanyName}</span> : ''
+            }
+            <label for="registerCompanyAddress">Address</label>
+            <input onChange={this.handleChange} name="registerCompanyAddress" id="registerCompanyAddress" className="formInput" type="text"></input>
+            {
+              formErrors.registerCompanyAddress ? <span className="errorMessage" data-error="wrong" data-success="right">{formErrors.registerCompanyAddress}</span> : ''
+            }
+            <label for="registerCompanyCity">City</label>
+            <input onChange={this.handleChange} name="registerCompanyCity" id="registerCompanyCity" className="formInput" type="text"></input>
+            {
+              formErrors.registerCompanyCity ? <span className="errorMessage" data-error="wrong" data-success="right">{formErrors.registerCompanyCity}</span> : ''
+            }
+            <label for="registerCompanyState">State</label>
+            <input onChange={this.handleChange} name="registerCompanyState" id="registerCompanyState" className="formInput" type="text"></input>
+            {
+              formErrors.registerCompanyState ? <span className="errorMessage" data-error="wrong" data-success="right">{formErrors.registerCompanyState}</span> : ''
+            }    
+            <label for="registerCompanyZip">Zip</label>
+            <input onChange={this.handleChange} name="registerCompanyZip" id="registerCompanyZip" className="formInput" type="text"></input>
+            {
+              formErrors.registerCompanyZip ? <span className="errorMessage" data-error="wrong" data-success="right">{formErrors.registerCompanyZip}</span> : ''
+            }
+            <button className="button">Submit</button>
+          </form>
       </div>
     )
   }
