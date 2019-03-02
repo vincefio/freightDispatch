baec9a03f200a5ea411cb7738f03ce6a2c1ab4e2
vincefio <vincentfiorilli@gmail.com>
2019-03-02 10:55:40 -0500

    register form

--- freight.react/src/components/RegisterCompany.js
+++ freight.react/src/components/RegisterCompany.js
@@ -1,39 +1,52 @@
 import React, { Component } from 'react'
 import firebase from 'firebase'
 
+
 export default class RegisterCompany extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      registerCompanyName: '',
+      registerCompanyAddress: '',
+      registerCompanyCity: '',
+      registerCompanyState: '',
+      registerCompanyZip: ''
+    }
+  }
+
   componentDidMount(){
      // Get a reference to the database service
     //var database = firebase.database();
     var database = firebase.firestore();
-
-    /*database.collection("companies").add({
-      first: "Ada",
-      last: "Lovelace",
-      born: 1815
-  })
-  .then(function(docRef) {
-      console.log("Document written with ID: ", docRef.id);
-  })
-  .catch(function(error) {
-      console.error("Error adding document: ", error);
-  });*/
-
-    /*database.ref('companies/' + 20).set({
-      name: 'Johnson',
-    });
-
-    console.log(database.ref('companies/'))*/
-
-  //MAKE A CLASS FOR A NEW COMPANY?  
   }
 
+  handleChange = (event) => {
+    console.log(event.target.value)
+    this.setState({
+      [event.target.name]: event.target.value
+    })
+  } 
+
+
   render() {
+    
     return (
       <div className="container">
         <h3>REGISTER COMPANY</h3>
           <label for="registerCompanyName">Company Name</label>
-          <input  name="registerCompanyName" id="registerCompanyName" className="" type="text"></input>
+          <input onChange={this.handleChange} name="registerCompanyName" id="registerCompanyName" className="" type="text"></input>
+          <label for="registerCompanyAddress">Address</label>
+          <input onChange={this.handleChange} name="registerCompanyAddress" id="registerCompanyAddress" className="formInput" type="text"></input>
+
+          <label for="registerCompanyCity">City</label>
+          <input onChange={this.handleChange} name="registerCompanyCity" id="registerCompanyCity" className="formInput" type="text"></input>
+
+          <label for="registerCompanyState">State</label>
+          <input onChange={this.handleChange} name="registerCompanyState" id="registerCompanyState" className="formInput" type="text"></input>
+   
+          <label for="registerCompanyZip">Zip</label>
+          <input onChange={this.handleChange} name="registerCompanyZip" id="registerCompanyZip" className="formInput" type="text"></input>
+
   
       </div>
     )
