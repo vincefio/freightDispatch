afa133e34b01ac0e964c378b9512037131132bc7
vincefio <vincentfiorilli@gmail.com>
2019-03-05 19:11:33 -0500

    firebase read

--- freight.react/src/components/Home.js
+++ freight.react/src/components/Home.js
@@ -2,10 +2,26 @@ import React, { Component } from 'react'
 import fire from '../config/firebase';
 import Truck from '../img/freightPic2.jpg';
 import { Link } from 'react-router-dom';
+import firebase from 'firebase';
 
 export default class Home extends Component {
     constructor(props){
         super(props);
+
+        var database = firebase.firestore();
+
+        var docRef = database.collection("companies").doc("chest");
+
+        docRef.get().then(function(doc) {
+            if (doc.exists) {
+                console.log("Document data:", doc.data());
+            } else {
+                // doc.data() will be undefined in this case
+                console.log("No such document!");
+            }
+        }).catch(function(error) {
+            console.log("Error getting document:", error);
+        });
 
         this.signOut = this.signOut.bind(this);
         //<img className="backgroundImg" src={Truck} alt="truck"></img>
